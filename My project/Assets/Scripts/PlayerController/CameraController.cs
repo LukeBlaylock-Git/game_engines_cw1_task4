a5ef2487939b67c833e2cab95ae76fa9f7d3ca5e
LancashireladOnGitHub <lukeblaylock35@gmail.com>
2025-12-06 00:53:05 +0000

    Creating the Player Controller, camera, etc
    Setting up the player, respawn point, etc, etc

---
+++ My project/Assets/Scripts/PlayerController/CameraController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Target;
+    public float Distance = 10f;
+    public float RotationSpeed = 2;
+    public float MinVertical = 0;
+    public float MaxVerticalAngle = 85;
+    public float ZoomSpeed = 5f;
+    public float MinZoom = 2f;
+    public float MaxZoom = 15f;
+
+
+
+    private Vector2 Rotation;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    // Update is called once per frame
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel"); //Gets the Input_Manager... input, uses it for our scroll variable.
+        Distance -= scroll * ZoomSpeed; //Distance subtract scroll * ZoomSpeed result
+        Distance = Mathf.Clamp(Distance, MinZoom, MaxZoom); //The clamp is to keep our camera between a Min and Max, hence why Min zoom and Max zoom are here, distance updates, min and max stay static.
+
+        Rotation += new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X") * RotationSpeed); //simply put Rotation moves the camera up and down depending on the mouse input, which is multplied for the rotation speed.
+        Rotation.x = Mathf.Clamp(Rotation.x, MinVertical, MaxVerticalAngle);
+
+        var TargetRotation = Quaternion.Euler(Rotation); //Bit confusing and this probably isnt accurate, but this takes Vector2 angles, converts them into a Quaternion, which is what unity uses to represent movement in a 3D space (kinda)
+
+        transform.position = Target.position - TargetRotation * new Vector3(0f, 0f, Distance);
+        transform.rotation = TargetRotation;
+
+
+    }
+}
---
+++ My project/Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+    Vector3 velocity;
+    float rotation_speed = 500f;
+
+    public float movementSpeed = 5.0f;
+    public float gravityMultiplier = 2;
+    public float JumpForce = 20;
+
+    private CharacterController character_controller;
+    private float downward_velocity;
+
+    public Transform Respawn;
+
+    void Start()
+    {
+        character_controller = GetComponent<CharacterController>();
+        Camera.main.GetComponent<CameraController>().Target = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal"); // inputs for Horizontal and vertical
+        vertical = Input.GetAxis("Vertical");
+        float move_amount = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+        velocity = new Vector3(horizontal, 0f, vertical) * movementSpeed;
+        velocity = Quaternion.LookRotation(new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z)) * velocity;
+
+
+
+        if (transform.position.y < -10f) //If the player goes below Y-10 then we respawn the player at the "Respawn" tag object
+        {
+            RespawnPlayer();
+        }
+        if (character_controller.isGrounded)
+        {
+            downward_velocity = -2f; //locks the player to the ground, good for slopes.
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                downward_velocity = JumpForce;
+            }
+        }
+        else
+        {
+            downward_velocity += Physics.gravity.y * gravityMultiplier * Time.deltaTime; //uses the rigidbody and inbuilt gravity system of unity.
+            //this is used so our player actually, comes down, not only that but so they dont feel "light" the gravity multipler is there to bring them back down with some force.
+        }
+
+        velocity.y = downward_velocity;
+        character_controller.Move(velocity * Time.deltaTime);
+
+
+        if (move_amount > 0)
+        {
+            var target_rotation = Quaternion.LookRotation(new Vector3(velocity.x, 0f, velocity.z));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target_rotation, 500f * Time.deltaTime); //This is just to make sure our character turns with our movement
+        }
+    }
+    public void RespawnPlayer()
+    {
+        character_controller.enabled = false;
+        transform.position = Respawn.position;
+        downward_velocity = 0f;
+        character_controller.enabled = true;
+    }
+}
